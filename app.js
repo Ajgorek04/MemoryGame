7f16658fb5abc107c36319aa90dcb5c864a32c2a
Ajgorek <igorblendzinski12@gmail.com>
2023-08-22 22:40:24 +0200

    Start adding logic to the game

---
+++ app.js
@@ -0,0 +1,30 @@
+const game = document.querySelector(".game");
+const startGameButton = document.querySelector("#startGame");
+const frontCards = document.querySelectorAll(".card-front");
+const cards = document.querySelectorAll(".card");
+const symbols = ["🎄", "❄️", "🧸", "🍪", "🃏"];
+
+startGameButton.addEventListener("click", () => {
+    startGameButton.parentElement.style.display = "none";
+    game.style.display = "flex";
+
+    const totalCards = cards.length;
+    const cardSymbols = [...symbols, ...symbols];
+
+    shuffleArray(cardSymbols);
+
+    frontCards.forEach((card, index) => {
+        card.textContent = cardSymbols[index];
+    });
+});
+
+// Tasowanie kart
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i);
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+//
